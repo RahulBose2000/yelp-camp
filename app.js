235b958493741451e02e9d2b3973aeb2c893ea9b
Rahul-か <rahulbose@myyahoo.com>
2024-11-12 11:19:53 +0530

    some minor changes

--- app.js
+++ app.js
@@ -26,11 +26,7 @@ const MongoStore = require('connect-mongo');
 // const dbURL = process.env.DB_URL;
 const dbUrl = process.env.DB_URL||"mongodb://localhost:27017/yelp-camp2";
 
-mongoose.connect(dbUrl,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-   
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,'connection errror:'));
